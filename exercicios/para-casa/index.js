03b3c3f901624a871ff7c30e668502f1757122cc
Skyy <alarconkell@gmail.com>
2022-08-20 18:39:50 -0300

    Primeiro exercicio

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -5,3 +5,10 @@ Solicite o preço de uma mercadoria e o percentual de desconto 20%.
 Exiba no console o valor do desconto e o preço a pagar.
 */
 
+const preco = 100
+const porcent = 20
+
+const venda = preco * (1 - porcent/100)
+const desconto = preco * porcent/100
+
+console.log (desconto, venda)
