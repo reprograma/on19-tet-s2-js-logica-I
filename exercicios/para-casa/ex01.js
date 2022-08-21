9e670fc43d47c5a9daa7a333823e4b636c3b977f
pâmela santos <pssantos201@gmail.com>
2022-08-21 14:15:02 -0300

    add home exercises

---
+++ exercicios/para-casa/ex01.js
@@ -0,0 +1,8 @@
+// 1 - Solicite o preço de uma mercadoria e o percentual de desconto 20%. 
+// Exiba no console o valor do desconto e o preço a pagar.
+
+let valorProduto = 500
+let desconto = 500 * 0.20
+let valorFinal = valorProduto - desconto
+
+console.log(`O produto de ${valorProduto.toFixed(2)} recebeu um desconto de 20% que equivale a ${desconto.toFixed(2)}. O valor a pagar é ${valorFinal.toFixed(2)}. `)
---
+++ exercicios/para-casa/ex02.js
@@ -0,0 +1,8 @@
+// 2 - Calcule o tempo de uma viagem de carro. Pergunte a distância a 
+// percorrer e a velocidade média esperada para a viagem.
+
+let velocidadeMediaEmKm = 95
+let distancia = 433
+let tempoDoPercurso = distancia / velocidadeMediaEmKm
+
+console.log(`O tempo de viagem entre São Paulo e Rio de Janeiro é de ${tempoDoPercurso.toFixed(2)} horas.`)
