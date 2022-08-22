82e05dca9dbe354523d5daf0443f5d578cdc22a0
Tayane Pereira Ramos <tayanep.ramos@gmail.com>
2022-08-22 16:29:00 -0300

    "feat(exercicios): criacao dos exercicios da semana 2"

---
+++ exercicios/para-casa/exercicio4.js
@@ -0,0 +1,8 @@
+const diasAlugado = 10
+const kmPercorrido = 34 
+
+const precoPorDia = 60
+const precoPorKM = 0.15
+
+const precoTotal = kmPercorrido * precoPorKM + diasAlugado * precoPorDia
+console.log('Preço total a pagar: ' + precoTotal)
---
+++ exercicios/para-casa/exercicio5.js
@@ -0,0 +1,7 @@
+const numero1 = 10
+const numero2 = 40
+const numero3 = 21
+
+const media = numero1 + numero2 + numero3 / 3
+
+console.log(media)
