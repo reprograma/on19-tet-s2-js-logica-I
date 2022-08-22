b8ecdfb81024db2a91720a7de205d51df9527b70
Mariana Cristina Dias dos Santos <marianacristinasantos05@gmail.com>
2022-08-21 22:46:35 -0300

    :sparkles: first exercise about variables in javascript

---
+++ exercicios/para-sala/exercicio01.js
@@ -0,0 +1,17 @@
+/*| Exemplo 01 | Descrição |
+| --- | --- |
+| `Variaveis` | Vamos criar variaveis usando let e const, atribuir valores, identificar comportamentos e saídas quando tentarmos recuperar. |*/
+
+const nome = "Mariana" // em regra, o meu nome é const, não pode ser mudado. Const é para usar um valor que não pode ser mudado no meu software, é uma medida de segurança
+
+let idade = 27
+
+let cidade = "Itararé"
+
+idade = 28 // por ser let é uma variavel que vai mudar 
+
+cidade = "São Paulo" // também posso me mudar de cidade 
+
+console.log(nome)
+console.log(idade)
+console.log(cidade)
