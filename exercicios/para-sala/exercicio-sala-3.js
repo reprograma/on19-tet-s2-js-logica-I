94c52726cf01c3072749943503baf7f479825ec7
Shirlene Teles <shiteles@gmail.com>
2022-08-22 15:49:02 -0300

    add segunda opção de resolução dos exercicios

--- exercicios/para-sala/exercicio-sala-3.js
+++ exercicios/para-sala/exercicio-sala-3.js
@@ -1,10 +1,19 @@
 // 3- Crie uma variavel e atribua como valor uma operacao de média entre 5 números inteiros e positivos.
 
-const n1 = 2
+/*const n1 = 2
 const n2 = 5
 const n3 = 6
 const n4 = 10
 const n5 = 8
 const media = (n1 + n2+ n3+ n4 + n5) / 5
 
-console.log(media)
+console.log(media)*/
+
+// Segunda opçãpo de resolução 
+
+let listaDeNotas = [8, 6, 9, 7, 10]
+let n = 5
+let soma = listaDeNotas[0] + listaDeNotas[1] + listaDeNotas[2] + listaDeNotas[3] + listaDeNotas[4]
+let media = soma / n
+
+ console.log(media)
