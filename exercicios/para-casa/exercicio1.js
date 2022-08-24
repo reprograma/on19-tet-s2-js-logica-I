96f41bee51412f14318ac822881924cf28641d02
sarah andrade <sarahpng828@gmail.com>
2022-08-23 21:33:55 -0300

    week 2 - exercises

---
+++ exercicios/para-casa/exercicio1.js
@@ -0,0 +1,9 @@
+const mercadoria = 2000
+
+const desconto = mercadoria *(20/100)
+
+const valorFinal = mercadoria - desconto
+
+const resposta = `o valor final é ${valorFinal} e o desconto ${desconto}`
+
+console.log(resposta)
---
+++ exercicios/para-casa/exercicio5.js
@@ -0,0 +1,9 @@
+const numeros = [9, 7, 9]
+
+let soma = (numeros[0] + numeros[1] + numeros[2])
+
+let n = 3
+
+let media = soma / n
+
+console.log(media)
