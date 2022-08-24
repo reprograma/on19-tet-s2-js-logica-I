08717b6829fe771cd9bd21485e8909a73403b95b
Lilin <cthulaura@gmail.com>
2022-08-24 20:29:56 -0300

    Adds files from first week of programming logic homework

---
+++ exercicios/para-casa/exercicio1.js
@@ -0,0 +1,8 @@
+// 1 - Solicite o preço de uma mercadoria e o percentual de desconto 20%. Exiba no console o valor do desconto e o preço a pagar.
+
+const productValue = 1830
+const discount = productValue*0.2
+const discountedPrice = productValue*0.8
+
+console.log(`Original value of the product was R$${productValue}. The value of a 20% discount is R$${discount}, with a final price of R$${discountedPrice}.`)
+
---
+++ exercicios/para-casa/exercicio5.js
@@ -0,0 +1,6 @@
+// 5 - Receba do usuários 3 números e calcule a média entre eles.
+
+const numbersInput = [23,77,135]
+const mean = (numbersInput[0] + numbersInput[1] + numbersInput[2]) / 3
+
+console.log(mean)
