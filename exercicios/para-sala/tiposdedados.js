423874a55717c1890c802d7bd7033ea279424621
judy <judysantos169@gmail.com>
2022-08-24 14:55:52 -0300

    exercisesAnswered-exercicio1.js

---
+++ exercicios/para-sala/tiposdedados.js
@@ -0,0 +1,52 @@
+
+
+// Exemplo 01 | Descrição |
+
+//| `Variaveis` | Vamos criar variaveis usando let e const, atribuir valores, identificar comportamentos e saídas quando tentarmos recuperar. |
+
+let nome = "judy"
+//| Exemplo 02 | Descrição |
+
+//| `Concatenar x interpolar` | Vamos criar variaveis usando let, const, atribuir strings e vamos concatenar e interpolar. |
+
+//| Exemplo 03 | Descrição |
+
+//| `Média entre números` | Crie uma variavel e atribua como valor uma operacao de média entre 5 números inteiros e positivos. |
+
+//| Exemplo 04 | Descrição |
+
+//| `Quero lucrar` | Crie um programa que me diga como lucrar nesta situação: quero vender uma bicicleta que 60% do seu valor é R$300, por quanto devo vendê-la para lucrar 25%? |
+
+//| Exemplo 05 | Descrição |
+
+//`Quero somar` | Faça um programa que peça dois números e imprima no console a soma desses dois números.|
+
+//| Exemplo 06 | Descrição |
+
+//| `Desapegada` | Crie um programa que me diga quanto cobrar para vender um notebook usado: o seu custo foi R$3000, por quanto devo vendê-lo descontando 25%? |
+
+// Exemplo 07 | Descrição |
+
+// `conversor de medidas` | Escreva um programa que receba um valor em metros e o exiba no console convertido em milímetros. |
+
+//| Exemplo 08 | Descrição |
+
+//`calculadora de IMC` | Escreva um programa que recebe uma altura em metros e o peso em kg e calcule o IMC. |
+
+// Exemplo 09 | Descrição |
+
+// `Boletim` | Escreva um programa que recebe 4 notas, calcula a média e exibe no console. |
+
+// Exemplo 10 | Descrição |
+
+//`conversor de temperatura` | Escreva um programa que recebe a temperatura em graus Celsius e converte para Fahrenheit, exiba o resultado no console. |
+
+/*Terminou o exercício? Dá uma olhada nessa checklist e confere se tá tudo certinho, combinado?!
+
+- [ ] Fiz o fork do repositório.
+- [ ] Clonei o fork na minha máquina (`git clone url-do-meu-fork`).
+- [ ] Resolvi o exercício.
+- [ ] Adicionei as mudanças. (`git add .` para adicionar todos os arquivos, ou `git add nome_do_arquivo` para adicionar um arquivo específico)
+- [ ] Commitei a cada mudança significativa ou na finalização do exercício (`git commit -m "Mensagem do commit"`)
+- [ ] Pushei os commits na minha branch (`git push origin nome-da-branch`)*/
+
