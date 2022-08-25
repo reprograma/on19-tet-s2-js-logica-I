795363df7a713acc8954b417570d409d876520c7
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-08-25 15:46:19 -0300

    add exercise 10 - temperature converter

---
+++ exercicios/para-sala/ex10.js
@@ -0,0 +1,7 @@
+// Exercício 10 - Conversor de temperatura
+
+// Escreva um programa que recebe a temperatura em graus Celsius e converte para Fahrenheit, exiba o resultado no console
+
+const tempCelsius = 40
+const formulaFahrenheit = 1.8 * tempCelsius + 32
+console.log(formulaFahrenheit)
