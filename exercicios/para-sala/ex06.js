0166340232d71529934dfa6aea5444e6e5a26053
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-08-25 15:43:47 -0300

    add exercise 06 - detached

---
+++ exercicios/para-sala/ex06.js
@@ -0,0 +1,8 @@
+// Exercício 06 - Desapegada
+
+// Crie um programa que me diga quanto cobrar para vender um notebook usado: o seu custo foi R$3000, por quanto devo vendê-lo descontando 25%?
+
+const custoNotebook = 3000
+const desconto = custoNotebook * 0.25
+const valorVenda = custoNotebook - desconto
+console.log(valorVenda)
