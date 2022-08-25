f00ac0aa242c365975b1980e6faf2928b44c7f1c
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-08-25 15:49:11 -0300

    add exercise 02 (home) - calculates the time of a trip

---
+++ exercicios/para-casa/ex02.js
@@ -0,0 +1,13 @@
+// Exercício de Casa 🏠
+
+// 02 - Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem
+
+console.log('Informe a distância a ser percorrida na viagem:')
+const distancia = 1200
+console.log(distancia + ' ' + 'km')
+console.log('Informe a velocidade média esperada:')
+const velocidade = 80
+console.log(velocidade + ' ' + 'km/h')
+tempo = distancia / velocidade
+console.log('O tempo de viagem será de aproximadamente:')
+console.log(tempo + ' ' + 'horas')
