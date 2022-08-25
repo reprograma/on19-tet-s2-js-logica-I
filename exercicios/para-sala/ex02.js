58df500cd4e9ceefe97a1a4f1f9a4e32b4e62885
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-08-25 15:41:37 -0300

    add exercise 02 - concatenate x interpolate

---
+++ exercicios/para-sala/ex02.js
@@ -0,0 +1,11 @@
+// Exercício 02 | Concatenar x interpolar
+
+// Vamos criar variaveis usando let, const, atribuir strings e vamos concatenar e interpolar
+
+const nome = 'Mellyssa'
+const nome2 = 'Stephanny'
+const nomeComposto = `${nome} ${nome2}`
+const curso = 'Reprograma - Todas em Tech (backend)'
+const apresentacao = `Olá, meu nome é ${nomeComposto} e sou aluna da ${curso}`
+
+console.log(apresentacao)
