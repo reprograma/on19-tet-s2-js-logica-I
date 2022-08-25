abb4aea2a32f2d44456a2a78a99ab4c01c5dfc53
Aurora Silva <aurorafsilva1@gmail.com>
2022-08-25 11:32:50 -0300

    Add the exercises

---
+++ exercicios/para-casa/Exercicio1.js
@@ -0,0 +1,11 @@
+//1 - Solicite o preço de uma mercadoria e o percentual de desconto 20%. Exiba no console o valor do desconto e o preço a pagar.//
+
+const valorMercadoria = 100
+
+const valorDesconto = 100 * 0.20
+
+const valorTotal = valorMercadoria - valorDesconto
+
+console.log(valorDesconto)
+
+console.log(valorTotal)
---
+++ exercicios/para-casa/Exercicio3.js
@@ -0,0 +1,11 @@
+//3 - Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
+
+const valorJantar = 200
+
+const taxaDoGarçom = 200 * 0.10
+
+const valorTotal = valorJantar - taxaDoGarçom
+
+console.log (taxaDoGarçom)
+
+console.log (valorTotal)
