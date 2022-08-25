32ca6d54175e3a44dcefa7e77c75ca2551715c03
Mariana Cristina Dias dos Santos <marianacristinasantos05@gmail.com>
2022-08-24 22:08:24 -0300

    :sparkles: exercise about math average done (exercise for home)

---
+++ exercicios/para-casa/exercicio-casa05.js
@@ -0,0 +1,7 @@
+/*5 - Receba do usuários 3 números e calcule a média entre eles.*/
+
+let numerosCalculoMedia = [9, 55, 178]
+
+let mediaEntreNumeros = (numerosCalculoMedia[0] + numerosCalculoMedia [1] + numerosCalculoMedia [2]) / 3
+
+console.log(mediaEntreNumeros)
