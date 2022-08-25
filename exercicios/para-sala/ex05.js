c8043aac2ec35eaef501d70413bd8d12daa80675
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-08-25 15:43:18 -0300

    add exercise 05 - I want to add

---
+++ exercicios/para-sala/ex05.js
@@ -0,0 +1,8 @@
+// Exercício 05 - Quero somar
+
+//Faça um programa que peça dois números e imprima no console a soma desses dois números
+
+const num1 = 50
+const num2 = 75
+const soma = num1 + num2
+console.log(soma)
