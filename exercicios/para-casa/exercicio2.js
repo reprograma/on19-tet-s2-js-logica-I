206dd5a2891cbfbb07a8d036aa71f7f30c0996d9
Camila Saldanha <camila.emrs@gmail.com>
2022-08-25 12:24:30 -0300

    Adicionar exercicio para casa

---
+++ exercicios/para-casa/exercicio2.js
@@ -0,0 +1,6 @@
+//2 - Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+
+let distancia = 100
+let velocidade = 50
+let tempoDaViagem = distancia / velocidade
+console.log(tempoDaViagem)
---
+++ exercicios/para-casa/exercicio5.js
@@ -0,0 +1,8 @@
+//5 - Receba do usuários 3 números e calcule a média entre eles.
+
+const numero1 = 15
+const numero2 = 7
+const numero3 = 20
+const media = (numero1 + numero2 + numero3) / 3
+
+console.log(media)
