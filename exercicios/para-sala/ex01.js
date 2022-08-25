4ebaaa5ea30c37307bae2176cb5fb7f5bd19b488
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-08-25 15:40:55 -0300

    add exercise 01 - variables

---
+++ exercicios/para-sala/ex01.js
@@ -0,0 +1,11 @@
+// Exercício 01 - Variaveis
+
+// Vamos criar variaveis usando let e const, atribuir valores, identificar comportamentos e saídas quando tentarmos recuperar
+
+const nome = 'Mellyssa'
+const curso = 'Sistemas de Informação'
+let cidade = 'Vitória'
+
+cidade = 'São Paulo'
+
+console.log(cidade)
