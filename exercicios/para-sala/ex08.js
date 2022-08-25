480831461c0b51d034c3c8aef19319ca87bcb2bf
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-08-25 15:45:07 -0300

    add exercise 08 - BMI calculator

---
+++ exercicios/para-sala/ex08.js
@@ -0,0 +1,8 @@
+// Exercício 08 - Calculadora de IMC
+
+// Escreva um programa que recebe uma altura em metros e o peso em kg e calcule o IMC
+
+altura = 1.75
+peso = 78
+calculoIMC = peso / (altura ^ 2)
+console.log(calculoIMC)
