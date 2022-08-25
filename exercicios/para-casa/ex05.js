15567e5740060a21e4d6c151b4aa149b976469a1
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-08-25 15:51:00 -0300

    add exercise 05 (homework) - averages 3 numbers

---
+++ exercicios/para-casa/ex05.js
@@ -0,0 +1,17 @@
+// Exercício de Casa 🏠
+
+// 05 - Receba do usuários 3 números e calcule a média entre eles
+
+console.log('Informe o primeiro número:')
+let num1 = 10
+console.log(num1)
+console.log('Informe o segundo número:')
+let num2 = 20
+console.log(num2)
+console.log('Informe o terceiro número:')
+let num3 = 30
+console.log(num3)
+
+const media = (num1 + num2 + num3) / 3
+
+console.log('A média dos números é:' + ' ' + media)
