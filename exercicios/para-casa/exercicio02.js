739277dfde300ad725eae228a22923d972b35670
Liliane Gomes <lilianemourag@gmail.com>
2022-08-24 21:46:21 -0300

    Adiciona exercícios

---
+++ exercicios/para-casa/exercicio02.js
@@ -0,0 +1,7 @@
+//Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+
+let distancia = 20
+let vlcmedia = 60
+let tempo = distancia / vlcmedia
+
+console.log(tempo)
---
+++ exercicios/para-sala/index.js
@@ -0,0 +1,8 @@
+//Crie uma variavel e atribua como valor uma operacao de média entre 5 números inteiros e positivos.
+
+let listaDeNotas = [8, 6, 9, 7, 10]
+let n = 5
+let soma = listaDeNotas[0] + listaDeNotas[1] + listaDeNotas[2] + listaDeNotas[3] + listaDeNotas[4]
+let media = soma / n
+
+console.log(media)
