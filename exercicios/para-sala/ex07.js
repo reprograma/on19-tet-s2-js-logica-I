23ed509222000e838a1e398817eeb97bb1ec6e30
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-08-25 15:44:56 -0300

    add exercise 07 - measurement converter

---
+++ exercicios/para-sala/ex07.js
@@ -0,0 +1,7 @@
+// Exercício 07 - Conversor de medidas
+
+// Escreva um programa que receba um valor em metros e o exiba no console convertido em milímetros
+
+const valorEmMetros = 5
+const conversaoMilimetro = valorEmMetros * 1000
+console.log(conversaoMilimetro)
