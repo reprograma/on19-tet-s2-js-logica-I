230d500a5824cbab4a91aecaeb87c580e615b66c
elvirabl <elvirabrunoleme@gmail.com>
2022-08-24 21:17:04 -0300

    Exercícios de Logica 1

---
+++ exercicios/para-casa/exercicio1.js
@@ -0,0 +1,8 @@
+//1 - Solicite o preço de uma mercadoria e o percentual de desconto 20%. Exiba no console o valor do desconto e o preço a pagar.
+
+const preco = 10
+const desconto = preco * 0.2
+console.log(desconto)
+
+const valorapagar = preco-desconto
+console.log(valorapagar)
---
+++ exercicios/para-casa/exercicio3.js
@@ -0,0 +1,7 @@
+//3 - Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
+
+const conta = 500
+const taxa = conta * 0.1
+const total = conta + taxa
+
+console.log(total)
