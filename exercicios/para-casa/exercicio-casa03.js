8cdc0d3ff00f999d5ab21a8e92a4c8167f192e44
Mariana Cristina Dias dos Santos <marianacristinasantos05@gmail.com>
2022-08-24 21:44:25 -0300

    :sparkles: exercise about waiter's tip done (exercise for home)

---
+++ exercicios/para-casa/exercicio-casa03.js
@@ -0,0 +1,10 @@
+/*3 - Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.*/
+
+let valorJantar = 300
+
+let taxaGarcom = (valorJantar / 100 ) * 10
+
+let valorFinal = valorJantar  + taxaGarcom
+
+console.log (taxaGarcom)
+console.log (valorFinal)
