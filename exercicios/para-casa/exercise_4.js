1967051fac92085b9cba31fc3be7548e2b523048
MargarethGalmier <109858071+MargarethGalmier@users.noreply.github.com>
2022-08-24 22:22:37 -0300

    Add files via upload
    Added exercises 1-5.

---
+++ exercicios/para-casa/exercise_4.js
@@ -0,0 +1,10 @@
+/* 4 - Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim como a quantidade de dias pelos 
+quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro custa 60,00 reais por dia e 0,15 centavos por km rodado. */
+
+const traveledKm = 1337
+const rentDays = 7
+const valuePerDay = 60
+const kmValue = 0.15
+const rentPrice = rentDays*dayValue + traveledKm*kmValue
+
+console.log(`For a rented car which traveled ${traveledKm} km over the course of ${daysRented} days rented, the final price consists of R$${rentPrice}.`) 
