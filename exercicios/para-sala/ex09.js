5e3370c7c05ce671dd9e1cc2c3ef607848f4ee68
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-08-25 15:45:46 -0300

    add exercise 09 - bulletin

---
+++ exercicios/para-sala/ex09.js
@@ -0,0 +1,7 @@
+// Exemplo 09 - Boletim
+
+// Escreva um programa que recebe 4 notas, calcula a média e exibe no console
+
+let notas = [10, 7.5, 9.4, 4.5]
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4
+console.log(media.toFixed(2))
